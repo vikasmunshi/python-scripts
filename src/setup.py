d82fc3a06aa4f8698da5440804560d413c9da6f7
Vikas Munshi <vikas.munshi@ing.com>
2018-05-24 14:12:09 +0200

    initial code for crypt

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  setup.py:
+#
+from setuptools import setup
+
+setup(
+    name='crypt',
+    version='0.0.1',
+    author='Vikas Munshi',
+    author_email='vikas.munshi@gmail.com',
+    url='',
+    description='secured secrets',
+    packages=['crypt']
+)
