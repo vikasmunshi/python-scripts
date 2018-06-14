085d6e3c503630ff088a8b9ef12669a57fe1725d
Vikas Munshi <vikas.munshi@ing.com>
2018-06-14 16:25:29 +0200

    python2 version of basic strategy

--- tic_tac_toe/strategies/basic2.py
+++ tic_tac_toe/strategies/basic2.py
@@ -5,43 +5,54 @@ from tic_tac_toe import *
 __author__ = 'Vikas Munshi'
 
 
-@mem_cached
+@memoize
 def find_center_cell_moves(board):
     return tuple([c for c in get_possible_moves(board) if is_center_cell(c, board.size)])
 
 
-@mem_cached
+@memoize
 def find_corner_cell_moves(board):
     return tuple([c for c in get_possible_moves(board) if is_corner_cell(c, board.size)])
 
 
-@mem_cached
+@memoize
 def find_defensive_moves(board):
     return tuple([c for c in get_possible_moves(board) if last_move_has_won(Board(board.size, board.moves + ((), c)))])
 
 
-@mem_cached
+@memoize
 def find_winning_moves(board):
     return tuple([c for c in get_possible_moves(board) if last_move_has_won(Board(board.size, board.moves + (c,)))])
 
 
-@mem_cached
+@memoize
+def get_first_move(board):
+    return () if board.moves else (Cell(1, 1),)
+
+
+@memoize
 def get_moves(board):
-    return find_winning_moves(board) or \
+    return get_first_move(board) or \
+           find_winning_moves(board) or \
            find_defensive_moves(board) or \
-           find_corner_cell_moves(board) or \
            find_center_cell_moves(board) or \
+           find_corner_cell_moves(board) or \
            get_possible_moves(board)
 
 
-@mem_cached
-def is_center_cell(cell, board_size) -> bool:
+@memoize
+def is_center_cell(cell, board_size):
     return cell.row_id not in (0, board_size - 1) and cell.col_id not in (0, board_size - 1)
 
 
-@mem_cached
+@memoize
 def is_corner_cell(cell, board_size) -> bool:
     return cell.row_id in (0, board_size - 1) and cell.col_id in (0, board_size - 1)
+
+
+@memoize
+def is_first_move(board) -> bool:
+    return not board.moves
 
 
 def strategy(board):
