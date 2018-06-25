36e5d91047309497d76d6aa42ffd15ece9fa7401
Vikas Munshi <vikas.munshi@ing.com>
2018-06-26 03:34:10 +0800

    removed tic_tac_toe

--- README.md
+++ README.md
@@ -4,26 +4,3 @@
 
     pip3 install -e git+https://github.com/vikasmunshi/python-scripts.git#egg=akira
 
-
-## tic_tac_toe
-
-### Usage:
-
-    python3 -m tic_tac_toe [-h] [-d STRATEGIES_FOLDER] [-g GAMES] [-t {fight,points}] [--include_bad] [--py2]
-    
-    optional arguments:
-      -h, --help            show this help message and exit
-      -b BOARD_SIZE         board size, default is 3
-      -d STRATEGIES_FOLDER  location of player strategy files, default is TIC_TAC_TOE_DIR/strategies
-      -g GAMES              number of games per match, default is 1000
-      -t {fight,points}     fight for elimination or play for points, default is elimination
-      --include_bad         include files matching bad*.py in strategies folder, ignored by default
-      --py2                 also load python 2 strategy files
-
-
-### Template for strategy
-
-    from tic_tac_toe import *
-    def strategy(board: Board) -> Cell:
-        # your code here
-        return Cell(0,0)
--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# setup.py
+#
+#  setup.py
+#
 from setuptools import setup
 
 setup(
         name='akira',
-        version='1.4.1',
+        version='0.0.1',
         author='Vikas Munshi',
         author_email='vikas.munshi@gmail.com',
-        url='https://github.com/vikasmunshi/python-scripts.git',
-        description='Poetry in Python',
-        packages=['tic_tac_toe', 'tic_tac_toe.strategies'],
-        package_dir={'tic_tac_toe': 'tic_tac_toe'},
-        package_data={'tic_tac_toe': 'tic_tac_toe/memory.json'},
-        install_requires=[],
+        url='https://github.com/vikasmunshi/python-scripts/tree/master/src/crypt',
+        description='package for persisting secrets securely as shares distributed across custodians',
+        packages=['crypt'],
+        package_dir={'crypt': 'src/crypt'},
+        package_data={'crypt': 'inventory/*.json'},
+        install_requires=['pycryptodome>=3.6.1', 'ifaddr>=0.1.4'],
         license='MIT License',
         platforms=['any'],
         long_description=open('README.md').read()
