4a61a8a2ee053ec1366b241f9259ca239544b1d1
Vikas Munshi <vikas.munshi@ing.com>
2019-07-16 14:34:15 +0200

    added graph analysis script for rca workshop

--- scripts/workshop_rca.py
+++ scripts/workshop_rca.py
@@ -24,9 +24,8 @@ def getfilename() -> str:
     return filename
 
 
-# noinspection PyPep8
 def read_transform_dump(filename: str = '', worksheet_name: str = '',
-                        id_field: str = '', description_field: str = '', caused_by_field: str = '',
+                        label_field: str = '', description_field: str = '', caused_by_field: str = '',
                         uri: str = default_uri, user: str = default_user, password: str = default_pwd,
                         clean_up: bool = False, show: bool = False) -> None:
     # read Excel file and load the contents of the given or last worksheet as data
@@ -34,47 +33,54 @@ def read_transform_dump(filename: str = '', worksheet_name: str = '',
         data, filename = workbook.parse(worksheet_name or workbook.sheet_names[-1]), workbook.io
 
     # filter unused columns, simplify names, & transform caused_by column into list
-    data = data[[id_field or data.columns[0], description_field or data.columns[1], caused_by_field or data.columns[2]]]
-    data.columns = ('id', 'description', 'caused_by')
+    data = data[[label_field or data.columns[0],
+                 description_field or data.columns[1],
+                 caused_by_field or data.columns[2]]]
+    data.columns = ('label', 'description', 'caused_by')
+
     # split values in caused_by column on newline and comma
-    splitter = lambda x: tuple(i.strip() for s in (l.strip().split(',') for l in x.splitlines()) for i in s)
+    def splitter(s):
+        return [item.strip() for sublist in (line.strip().split(',') for line in s.splitlines()) for item in sublist]
+
     data.caused_by = data.caused_by.fillna('').apply(splitter)
 
     # connect to Neo4j instance
     with GraphDatabase.driver(uri, auth=basic_auth(user, password)) as driver:
         # wrapper for executing  parameterized cypher query as a transaction
-        def graph_transaction(cypher_query, **cypher_query_args):
+        def run_graph_trx(cypher_query, **cypher_query_args):
             with driver.session() as session:
                 session.write_transaction(lambda tx: tx.run(cypher_query, **cypher_query_args))
 
         # clear up if asked to
         if clean_up:
-            graph_transaction('MATCH (n:Issues) DETACH DELETE n')
+            run_graph_trx('MATCH (n:Issues) DETACH DELETE n')
 
         # add nodes
         add_nodes = 'MERGE (i:%s:Issues {label:$label}) SET i.description = $description'
-        classify = lambda r: 'Issue' if r.id.endswith('.00') else 'UnderlyingCause' if r.caused_by else 'RootCause'
-        data.apply(lambda r: graph_transaction(add_nodes % classify(r), label=r.id, description=r.description), axis=1)
+
+        def classifier(row):
+            return 'Issue' if row.label.endswith('.00') else 'UnderlyingCause' if row.caused_by else 'RootCause'
+
+        data.apply(lambda r: run_graph_trx(add_nodes % classifier(r), label=r.label, description=r.description), axis=1)
 
         # add relations
         add_relation = 'MATCH (i), (c) WHERE i.label = $label AND c.label = $cause MERGE (i)-[r:CausedBy]->(c)'
-        data.apply(lambda r: [graph_transaction(add_relation, label=r.id, cause=c) for c in r.caused_by], axis=1)
+        data.apply(lambda r: [run_graph_trx(add_relation, label=r.label, cause=c) for c in r.caused_by], axis=1)
 
         # calculate rank and add as score
         add_score = 'CALL algo.pageRank("", "", {iterations:25, dampingFactor:0.75, write:true, writeProperty:"score"})'
-        graph_transaction(add_score)
+        run_graph_trx(add_score)
 
         # query results sorted by score and read as DataFrame
-        query = 'MATCH (c)-[:CausedBy]->(i) RETURN i.label AS id, COLLECT(c.label) as leads_to, i.score as score'
+        q = 'MATCH (c)-[:CausedBy]->(i) RETURN i.label AS label, COLLECT(c.label) as leads_to, i.score * 100 as score'
         with driver.session() as query_session:
-            results = pd.DataFrame(query_session.run(query).values())
+            results = pd.DataFrame(query_session.run(q).values())
 
-    # value conversions plus join with data and save as Excel
-    results.columns = ('id', 'leads_to', 'score')
-    results.leads_to = results.leads_to.apply(tuple)
-    results.score = (results.score * 100).astype(int)
-    results = results.set_index('id')
-    results = data.join(results, on='id').sort_values(by=['score'], ascending=False).applymap(lambda val: val or '')
+    # join results with data plus list to string conversions and save as Excel
+    results.columns = ('label', 'leads_to', 'score')
+    results = data.join(results.set_index('label'), on='label').sort_values(by=['score'], ascending=False).fillna('')
+    results.caused_by = results.caused_by.apply('\n'.join)
+    results.leads_to = results.leads_to.apply('\n'.join)
     results.to_excel('%s_graph_output%s' % (splitext(filename)), index=False)
 
     # print results if asked for
@@ -88,7 +94,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='read, transform, and dump root cause analysis workshop output')
     parser.add_argument('-f', '--filename', default='', help='workshop results file')
     parser.add_argument('--worksheet_name', default='', help='worksheet name, defaults to last worksheet in workbook')
-    parser.add_argument('--id_field', default='', help='column to use for id, defaults to first column')
+    parser.add_argument('--label_field', default='', help='column to use for label, defaults to first column')
     parser.add_argument('--description_field', default='', help='column for description, defaults to second column')
     parser.add_argument('--caused_by_field', default='', help='column to use for caused_by, defaults to third column')
     parser.add_argument('--uri', default=default_uri, help='neo4j URI, default %s' % default_uri)
